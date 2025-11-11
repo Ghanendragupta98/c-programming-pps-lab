3e0f348ed54e6be82c50bc36506c382c63eb2795
Ghanendragupta98 <ghanendragupta98@gmail.com>
2025-11-11 17:30:16 +0530

    first program updated

--- ques13.c
+++ ques13.c
@@ -5,6 +5,12 @@ int main()
     
     printf("Enter a number: ");
     scanf("%d",&number);
+
+    if (number==0)
+    {
+    printf("Factorial of  %d is %d",number,factorial);
+    break;
+    }
     
     for(i=1;i<=number;i++)
     {
